44853546129fe67cf647080c810f5395590680e5
DanikingRD <danikingrd@gmail.com>
2023-01-05 07:51:33 -0400

    setup project

---
+++ src/main/java/daniking/deathtimer/DeathTimer.java
@@ -0,0 +1,13 @@
+package daniking.deathtimer;
+
+import net.fabricmc.api.ModInitializer;
+
+public class DeathTimer implements ModInitializer {
+
+    public static final String MOD_ID = "deathtimer";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
